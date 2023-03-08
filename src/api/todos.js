170838396e39e20ed08824d17596f75724b48953
James <mirageapache@gmail.com>
2023-03-08 15:27:59 +0800

    revise crud api

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,14 +1,30 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3001';
+const baseURL = 'https://todo-list.alphacamp.io/api';
+//新增 axios instance
+const axiosInstance = axios.create({ baseURL: baseURL });
+
+// 在axios發送request前將authToken設定於Header中傳送
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    console.error(error);
+  },
+);
 
 // GET (取得Todos資料)
 export const getTodos = async () => {
   try {
     // 用get的方式串接，並將response的值放到"res"變數內
-    const res = await axios.get(`${baseURL}/todos`);
+    const res = await axiosInstance.get(`${baseURL}/todos`);
     // 回傳取得的資料
-    return res.data;
+    return res.data.data;
   } catch (error) {
     console.error('[Get Todos failed]: ', error);
   }
@@ -21,7 +37,7 @@ export const createTodo = async (payload) => {
   const { title, isDone } = payload;
   try {
     // 用post的方法，(參數1是路徑，參數2是要傳的值)
-    const res = await axios.post(`${baseURL}/todos`, { title, isDone });
+    const res = await axiosInstance.post(`${baseURL}/todos`, { title, isDone });
     return res.data;
   } catch (error) {
     console.error('[Create Todo failed]: ', error);
@@ -32,7 +48,7 @@ export const createTodo = async (payload) => {
 export const patchTodo = async (payload) => {
   const { id, title, isDone } = payload;
   try {
-    const res = await axios.patch(`${baseURL}/todos/${id}`, {
+    const res = await axiosInstance.patch(`${baseURL}/todos/${id}`, {
       title,
       isDone,
     });
@@ -46,7 +62,7 @@ export const patchTodo = async (payload) => {
 // DELETE (刪除Todo資料)
 export const deleteTodo = async (id) => {
   try {
-    const res = await axios.delete(`${baseURL}/todos/${id}`);
+    const res = await axiosInstance.delete(`${baseURL}/todos/${id}`);
     return res.data;
   } catch (error) {
     console.error('[Delete Todo failed]: ', error);
